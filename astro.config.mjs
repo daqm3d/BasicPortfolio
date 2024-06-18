8117a03c46b6fc242f5298729581c1e207c1c535
daqm3d <daqm3d@gmail.com>
2024-06-18 12:34:22 -0400

    configuracion de archivo astro.config para github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://daqm3d.github.io',
+  //base: 'mi-repo',
   server: {
     host: true,
     hmr: {

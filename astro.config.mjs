b644a62c311789e5e7395e874a43c170adf85a40
daqm3d <daqm3d@gmail.com>
2024-06-18 23:30:54 -0400

    Revert "configuracion de archivo astro.config para github actions pruba"
    This reverts commit c406990abc39c26dab61fde9951f45bb97d89b51.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,12 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://daqm3d.github.io',
+  //base: 'mi-repo',
   server: {
     host: true,
     hmr: {
       //            host: 'IP'
     },
-  },
-  build: {
-    site: 'https://daqm3d.github.io',
-    base: 'BasicPortfolio',
   },
 });

ac33367b98efb351b9e0936f4ce905b4af7af46d
daqm3d <daqm3d@gmail.com>
2024-06-18 23:17:18 -0400

    Revert "modificado archivo astro.config 2"
    This reverts commit 467fcd0944c78d56abd9364c7a0158eca28cc9f7.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  build: {
-    site: 'https://daqm3d.github.io',
-    base: 'BasicPortfolio',
-  },
   server: {
     host: true,
     hmr: {
       //            host: 'IP'
     },
+  },
+  build: {
+    site: 'https://daqm3d.github.io',
+    base: 'BasicPortfolio',
   },
 });

09d4b4d5f8f4813c3f7accb7069d110c8ec4be00
daqm3d <daqm3d@gmail.com>
2024-06-26 18:48:45 -0400

    configurando archivo astro prueba :blush:

---
+++ astro.config.build.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://daqm3d.github.io',
+  base: 'BasicPortfolio',
+});
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,11 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://daqm3d.github.io',
-  base: 'BasicPortfolio',
   server: {
     host: true,
     /*
     hmr: {
       //            host: 'IP'
     },*/
-    devOptions: {
-      enabled: true,
-      site: 'https://daqm3d.github.io',
-      base: 'D',
-    },
-    buildOptions: {
-      site: 'https://daqm3d.github.io',
-      base: 'B',
-    },
   },
 });
